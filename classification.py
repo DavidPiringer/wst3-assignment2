29df3729f059be9d0a4e91c976c50c4bf672cb9b
David Piringer <s1910454024@students.fh-hagenberg.at>
2020-12-04 14:18:16 +0100

    update classifcation.py

--- classification.py
+++ classification.py
@@ -5,11 +5,18 @@ import string
 import csv
 import spacy
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 
-def readInput(input, delimiter):
+def readInput(file, delimiter):
+    input = open(str(file), 'r')
     dataset = []
     lines = csv.reader(input, delimiter=delimiter)
     for line in lines:
         dataset.append(line)
+    input.close()
     return dataset
+
+
+ds = readInput('CtD.csv', ';')
+print(ds)
